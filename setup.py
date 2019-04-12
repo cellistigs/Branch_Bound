4ad5702ff9c45de63aefbded189959234bea7ba9
Sean Bittner <sbittner@Seans-MacBook-Pro.local>
2019-04-11 20:38:59 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='git_template',
+      version='0.1',
+      description='Git template.',
+      author='Sean Bittner',
+      author_email='srb2201@columbia.edu',
+      install_requires=['numpy'],
+      packages=['git_template']
+     )
